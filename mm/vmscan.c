c486f378a9bafcecb3223ff4f768be55aa5a2ea2
Renaud Allard <renaud@allard.it>
2011-02-08 17:32:57 +0100

    Fugu logo Changes in syncronous waits for writeback

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1219,7 +1219,7 @@ static unsigned long shrink_inactive_list(unsigned long max_scan,
 		nr_freed = shrink_page_list(&page_list, sc, PAGEOUT_IO_ASYNC);
 
 		/* Check if we should syncronously wait for writeback */
-		if (should_reclaim_stall(nr_taken, nr_freed, priority,
+		if (should_reclaim_stall(nr_taken, nr_reclaimed, priority,
 					lumpy_reclaim, sc)) {
 			congestion_wait(BLK_RW_ASYNC, HZ/10);
 
