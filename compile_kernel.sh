6d194952dd358cc7bca513a25b9f21a4f6a69c53
Renaud Allard <renaud@allard.it>
2011-03-04 13:02:39 +0100

    put modules in the right place

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -2,5 +2,5 @@
 . ./setenv.sh
 make -i clean
 make -j4
-find . -name *.ko | while read MODULE; do cp $MODULE ../initramfs/lib/modules/ ; done
+find . -name *.ko | while read MODULE; do cp $MODULE ../initramfs/roo/lib/modules/ ; done
 make -j4
