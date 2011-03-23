7ed262d328976592dd58974dd0858fe50a056ab2
Renaud Allard <renaud@allard.it>
2011-03-23 10:58:04 +0100

    Add some missing but needed files

---
+++ include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 306 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#306 Wed Mar 23 10:06:13 CET 2011"
+#define LINUX_COMPILE_TIME "10:06:13"
+#define LINUX_COMPILE_BY "r"
+#define LINUX_COMPILE_HOST "bilbo"
+#define LINUX_COMPILE_DOMAIN "eriador.org"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
