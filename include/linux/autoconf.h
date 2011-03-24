eb2562d9de7598d08ef47f771c63e59ecd8451ed
Renaud Allard <renaud@allard.it>
2011-03-24 12:30:14 +0100

    Use 2010q1 toolchain

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.9
- * Fri Mar 18 14:07:29 2011
+ * Thu Mar 24 12:17:12 2011
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_MMC_SDHCI_S3C_DMA 1
