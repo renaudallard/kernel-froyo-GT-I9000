24263640e40c7e5a1eb588b88ec3174580495697
Renaud Allard <renaud@allard.it>
2011-04-04 18:54:56 +0200

    revert setenv

--- setenv.sh
+++ setenv.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 export ARCH=arm 
-export PATH=$PATH:/home/r/arm-2010.09/bin:/usr/java/jdk1.6.0_21/bin
-export CROSS_COMPILE=/home/r/arm-2010.09/bin/arm-none-linux-gnueabi-
+export PATH=$PATH:/home/r/arm-2010q1/bin:/usr/java/jdk1.6.0_21/bin
+export CROSS_COMPILE=/home/r/arm-2010q1/bin/arm-none-linux-gnueabi-
 export ac_cv_linux_vers=2.6.32
-export CC=/home/r/arm-2010.09/bin/arm-none-linux-gnueabi-gcc
+export CC=/home/r/arm-2010q1/bin/arm-none-linux-gnueabi-gcc
 #export CC=/home/r/galaxy/agcc
-export LD=/home/r/arm-2010.09/bin/arm-none-linux-gnueabi-ld
-export AR=/home/r/arm-2010.09/bin/arm-none-linux-gnueabi-ar
-export RANLIB=/home/r/arm-2010.09/arm-none-linux-gnueabi/bin/ranlib
-export PATH=$PATH:/home/r/arm-2010.09/bin
-export PATH=$PATH:/home/r/arm-2010.09/
-export PATH=$PATH:/home/r/arm-2010.09/lib
+export LD=/home/r/arm-2010q1/bin/arm-none-linux-gnueabi-ld
+export AR=/home/r/arm-2010q1/bin/arm-none-linux-gnueabi-ar
+export RANLIB=/home/r/arm-2010q1/arm-none-linux-gnueabi/bin/ranlib
+export PATH=$PATH:/home/r/arm-2010q1/bin
+export PATH=$PATH:/home/r/arm-2010q1/
+export PATH=$PATH:/home/r/arm-2010q1/lib
 #export CFLAGS="-static -Os -mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp -fno-gcse -fprefetch-loop-arrays --param l2-cache-size=512 --param l1-cache-size=64 --param simultaneous-prefetches=6 --param prefetch-latency=400 --param l1-cache-line-size=64"
 export CFLAGS="-static -Os -fstack-protector -fstack-protector-all"
 export LDFLAGS="-static -Os"
